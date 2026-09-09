4aedde3893e2c721752e83976fee0fe906e61544
Trần Lê Nam Khánh <~tranlenamkhanh2k5@gmail.com>
2026-09-09 14:20:55 +0700

    feat: add finding user service for user-service

--- src/BE/user-service/src/main/java/com/threecats/userservice/exception/UserNotFoundException.java
+++ src/BE/user-service/src/main/java/com/threecats/userservice/exception/UserNotFoundException.java
@@ -1,4 +1,19 @@
 package com.threecats.userservice.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends BaseException {
+
+    // 1. Constructor không tham số (dùng message mặc định)
+    public UserNotFoundException() {
+        super(404, "User not found");
+    }
+
+    // 2. Constructor nhận message tùy chỉnh
+    public UserNotFoundException(String message) {
+        super(404, message);
+    }
+
+    // 3. Constructor tiện lợi khi tìm theo ID/Email/Phone
+    public UserNotFoundException(String fieldName, Object fieldValue) {
+        super(404, String.format("User not found with %s: '%s'", fieldName, fieldValue));
+    }
 }
